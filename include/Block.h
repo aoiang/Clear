e8d374af2df691adfb62972d68e25fab6963f2c6
iechevarria <iechevarria@email.wm.edu>
2017-10-22 21:32:25 -0400

    fix Block.h, Block.cpp

--- include/Block.h
+++ include/Block.h
@@ -11,6 +11,8 @@
 
 class Block {
     protected:
+        float x;
+        float y;
         float length=80;
         float width=80;
         float speed=30;
@@ -27,15 +29,21 @@ class Block {
         float get_length();
         float get_width();
         float get_speed();
+        float getX();
+        float getY();
         int get_id();
         bool get_selected();
 
+        void setX(float x);
+        void setY(float y);
+        void setPosition(float x, float y);
         void set_length(float length);
         void set_width(float width);
         void set_speed(float speed);
         void set_id(int id);
         void init(int id);
         void set_selected(bool is_selected);
+        void move(float x, float y);
         virtual bool get_combine();
         virtual void set_combine(bool can_combine);
 
