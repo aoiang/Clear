46f92911325fc703aa70fbd461d7b322538003f7
iechevarria <iechevarria@email.wm.edu>
2017-11-06 15:55:46 -0500

    Refactor GameView

--- src/GameView.cpp
+++ src/GameView.cpp
@@ -7,32 +7,68 @@
 
 
 /**
- * constructor function
- * create the window
+ * Create the game window
  */
 GameView::GameView(float length, float width) :
     App(sf::VideoMode(length, width, 32), "Clear",  sf::Style::Default){}
 
 
 /**
- * check it window is open
+ * Check if window is open
  * @return bool
  */
 bool GameView::isOpen() {
     return App.isOpen();
 }
 
-/**
-* Updates the view at each frame
-*/
-void GameView::update() {
-  check_mousePosition(state -> get_board_array());
-  check_keyboard_in();
-  draw(state -> get_board_array());
-}
 
 /**
- * initialize all blocks, set all parameters for blocks
+* Updates the view and state at each frame
+*/
+void GameView::update() {
+  check_mouse_position();
+  check_keyboard_in();
+  draw();
+}
+
+
+/**
+* Assigns a GameState to this view
+* @param state is the GameState associated with this view
+*/
+void GameView::set_GameState(GameState &state) {
+    this -> state = &state;
+}
+
+
+/**
+* Make block shapes based on their properties
+* @param Normal_Block to draw
+* @return sf::RectangleShape
+*/
+sf::RectangleShape GameView::make_block_shape(Normal_Block block) {
+    sf::RectangleShape block_shape(sf::Vector2f(block.get_length(), block.get_width()));
+    block_shape.setFillColor(sf::Color::White);
+
+    if (block.get_has_direction()) {
+        if (block.get_direction() == "up") {
+            texture[block.get_id()].loadFromFile("../resources/up.png");
+        } else if (block.get_direction() == "down") {
+            texture[block.get_id()].loadFromFile("../resources/down.png");
+        } else if (block.get_direction() == "left") {
+            texture[block.get_id()].loadFromFile("../resources/left.png");
+        } else if (block.get_direction() == "right") {
+            texture[block.get_id()].loadFromFile("../resources/right.png");
+        }
+        block_shape.setTexture(&texture[block.get_id()]);
+    }
+
+    return block_shape;
+}
+
+
+/**
+ * Initialize all blocks, set all parameters for blocks
  */
 void GameView::block_init() {
     for(int i=0; i<5; i++){
@@ -40,75 +76,40 @@ void GameView::block_init() {
     }
 }
 
-void GameView::set_GameState(GameState &state) {
-    this -> state = &state;
-}
 
 /**
- * Draw blocks in the game based on their properties (like direction)
- * @param i for id
- * @return sf::RectangleShape
- */
-sf::RectangleShape GameView::directed_block_generator(Normal_Block *board_array, int i) {
-    if(board_array[i].get_has_direction()){
-        if(board_array[i].get_direction() == "up"){
-            texture[board_array[i].get_id()].loadFromFile("../resources/up.png");
-        }
-        if(board_array[i].get_direction() == "down"){
-            texture[board_array[i].get_id()].loadFromFile("../resources/down.png");
-        }
-        if(board_array[i].get_direction() == "left"){
-            texture[board_array[i].get_id()].loadFromFile("../resources/left.png");
-        }
-        if(board_array[i].get_direction() == "right"){
-            texture[board_array[i].get_id()].loadFromFile("../resources/right.png");
-        }
-
-        sf::RectangleShape c(sf::Vector2f(board_array[i].get_length(), board_array[i].get_width()));
-        c.setFillColor(sf::Color::White);
-        c.setTexture(&texture[board_array[i].get_id()]);
-        return c;
-    }
-    else{
-        sf::RectangleShape c(sf::Vector2f(board_array[i].get_length(), board_array[i].get_width()));
-        c.setFillColor(sf::Color::White);
-        return c;
-    }
-}
-
-
-/**
- * initialize the game, set all blocks.
+ * Initialize the game, set all blocks shapes
  * @return sf::RectangleShape
  */
 void GameView::init() {
     block_init();
-    sf::RectangleShape * b;
-    b = new sf::RectangleShape[5];
+    sf::RectangleShape * shapes;
+    shapes = new sf::RectangleShape[5];
     for(int i=0; i<5; i++) {
-        b[i] = directed_block_generator(state->get_board_array(), i);
-        b[i].setPosition(state->get_board_array()[i].getX(), state->get_board_array()[i].getY());
+        shapes[i] = make_block_shape(state -> get_board_array()[i]);
+        shapes[i].setPosition(state -> get_board_array()[i].getX(), state -> get_board_array()[i].getY());
     }
-    this -> block_shapes = b;
+    this -> block_shapes = shapes;
 }
+
 
 /**
 * Checks if mouse has clicked on a block
 */
-void GameView::check_mousePosition(Normal_Block * board_array) {
+void GameView::check_mouse_position() {
     int current_y = App.getSize().y;
     int current_x = App.getSize().x;
     for(int i=0; i<5; i++) {
-        if (sf::Mouse::getPosition(App).x >= int((board_array[i].getX() / 800) * current_x) && sf::Mouse::getPosition(App).x <= int(((board_array[i].getX()+100) / 800) * current_x)){
-            if (sf::Mouse::getPosition(App).y >= int((board_array[i].getY() / 600) * current_y) && sf::Mouse::getPosition(App).y <= int(((board_array[i].getY()+100) / 600) * current_y)){
+        if (sf::Mouse::getPosition(App).x >= int((state -> get_board_array()[i].getX() / 800) * current_x) && sf::Mouse::getPosition(App).x <= int(((state -> get_board_array()[i].getX()+100) / 800) * current_x)){
+            if (sf::Mouse::getPosition(App).y >= int((state -> get_board_array()[i].getY() / 600) * current_y) && sf::Mouse::getPosition(App).y <= int(((state -> get_board_array()[i].getY()+100) / 600) * current_y)){
                 if(sf::Mouse::isButtonPressed(sf::Mouse::Left)){
-                    for(int be_selected=0; be_selected<5; be_selected++){
-                        if(be_selected==i){
+                    for(int is_selected=0; is_selected<5; is_selected++){
+                        if(is_selected==i){
                             continue;
                         }
-                        board_array[be_selected].set_selected(false);
+                        state -> get_board_array()[is_selected].set_selected(false);
                     }
-                    board_array[i].set_selected(true);
+                    state -> get_board_array()[i].set_selected(true);
                 }
             }
         }
@@ -122,14 +123,14 @@ void GameView::check_mousePosition(Normal_Block * board_array) {
 */
 int GameView::draw_selected_block(Normal_Block * board_array) {
     int selected_block = 0;
-    for(int be_selected=0; be_selected<5; be_selected++){
-        if(!board_array[be_selected].get_selected()){
-            block_shapes[be_selected].setOutlineThickness(0);
+    for(int is_selected=0; is_selected<5; is_selected++){
+        if(!board_array[is_selected].get_selected()){
+            block_shapes[is_selected].setOutlineThickness(0);
         }
         else{
-            block_shapes[be_selected].setOutlineThickness(3.5);
-            block_shapes[be_selected].setOutlineColor(sf::Color::Red);
-            selected_block = be_selected;
+            block_shapes[is_selected].setOutlineThickness(3.5);
+            block_shapes[is_selected].setOutlineColor(sf::Color::Red);
+            selected_block = is_selected;
         }
     }
     return selected_block;
@@ -151,15 +152,15 @@ void GameView::draw_movement(Normal_Block * board_array) {
  * Draw all blocks, movements, and selections
  * @param board_array for array of blocks
  */
-void GameView::draw(Normal_Block * board_array) {
+void GameView::draw() {
     poll_event();
     App.clear(sf::Color(66, 150, 246));
     for (int i = 0; i < 5; i++) {
       App.draw(block_shapes[i]);
     }
     // Draw outlined selected block on top so the outline is on top
-    int selected_block = draw_selected_block(board_array);
-    draw_movement(board_array);
+    int selected_block = draw_selected_block(state -> get_board_array());
+    draw_movement(state -> get_board_array());
     App.draw(block_shapes[selected_block]);
     App.display();
 }
