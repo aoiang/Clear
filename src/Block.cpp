68762f3673729f638de04c8c6aae64f3770b5505
Patrick Collins <patrickcp0@gmail.com>
2017-11-12 02:38:52 +0000

    misc formatting and fixes fixed bug in move; misnamed variable init now calls set_id instead of repeating its code.

--- src/Block.cpp
+++ src/Block.cpp
@@ -7,57 +7,32 @@
 #include "Block.h"
 
 
-/**
-  @return block id
-*/
-int Block::get_id(){
-    return id;
-}
-
+/**@return block id*/
+int Block::get_id(){return id;}
 
 /**
   Sets block id
   @param id
 */
-void Block::set_id(int id) {
-    this ->id = id;
-}
+void Block::set_id(int id) {this->id = id;}
 
 
-/**
-  Initializes block, sets id
-  @param id
-*/
-void Block::init(int id) {
-    this ->id = id;
-}
+/**Initializes block, sets id*/
+void Block::init(int id) {set_id(id);}
 
 
-/**
-  Sets block x and y indices on the board
-  @param x
-  @param y
-*/
+/**Sets block x and y indices on the board*/
 void Block::setPosition(float row, float col) {
     this -> row = row;
     this -> col = col;
 }
 
 
-/**
-  @return x index of block on board
-*/
-float Block::get_row() {
-    return row;
-}
+/**@return x index of block on board*/
+float Block::get_row() {return row;}
 
-
-/**
-  @return y index of block on board
-*/
-float Block::get_col() {
-    return col;
-}
+/**@return y index of block on board*/
+float Block::get_col() {return col;}
 
 
 /**
@@ -65,24 +40,15 @@ float Block::get_col() {
   @param x units to move
   @param y units to move
 */
-void Block::move(int row, int y) {
-    this -> row += row;
-    this -> col += col;
+void Block::move(int x, int y) {
+    this->row += x;
+    this->col += y;
 }
 
 
-/**
-  @return can_combine
-*/
-bool Block::get_combine() {
-    return can_combine;
-}
+/**@return can_combine*/
+bool Block::get_combine() {return can_combine;}
 
 
-/**
-  Sets if Block can combine
-  @param can_combine
-*/
-void Block::set_combine(bool can_combine) {
-    this -> can_combine = can_combine;
-}
+/**Sets if Block can combine*/
+void Block::set_combine(bool can_combine) {this->can_combine = can_combine;}
