0dbfd1666ab6f851d2bd336feb1cc1057c3da622
Patrick Collins <patrickcp0@gmail.com>
2017-11-12 02:32:58 +0000

    add short comments

--- bin/Clear.cpp
+++ bin/Clear.cpp
@@ -8,8 +8,8 @@
 #include "GameView.h"
 
 int main(int argc, char** argv) {
-    int length = 800;
-    int width = 600;
+    int length = 800;//TODO make this a global var or input
+    int width = 600;//TODO make this a global var or input
     GameView view(length, width);
     GameLogic logic;
     logic.init();
