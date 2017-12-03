6515411d7979a56ba4ee7f0216d9d9f864ff936a
aoiang <zyyaoiang@gmail.com>
2017-12-03 17:50:30 -0500

    fix segfault after finished infinite mode

--- src/GameView_Screen.cpp
+++ src/GameView_Screen.cpp
@@ -350,6 +350,7 @@ int *GameView_Screen::run(sf::RenderWindow &window, int curr_level) {
     if (curr_level == -1){
         BoardGenerator * generator = new BoardGenerator();
         board = generator->make_board(logic->get_generated_board_x(), logic->get_generated_board_y());
+        re[1] = 0;
     }
     else{
         board = new BoardState(levels[re[1]-1]);
