9dbdae0f26ef6af2c1bbcad93b17de6abd6eee72
iechevarria <iechevarria@email.wm.edu>
2017-12-03 16:20:41 -0500

    Add GameLogic to MainMenu_Screen

--- include/MainMenu_Screen.hpp
+++ include/MainMenu_Screen.hpp
@@ -2,23 +2,26 @@
 #define CLEAR_MAINMENU_SCREEN_HPP
 
 #define MAX_NUM_OF_ITEMS 3
-#include <SFML/Graphics.hpp>
-#include <iostream>
+
 #include "Screen_Abstract.hpp"
 #include "BoardState.hpp"
+#include "GameLogic.hpp"
+#include <SFML/Graphics.hpp>
+#include <iostream>
 
 class MainMenu_Screen : public Screen_Abstract
 {
     private:
         sf::Text * title;
         sf::Text * menuItems;
+        GameLogic * logic;
     public:
-        void draw(sf::RenderWindow &window);
-        bool check_mousover(sf::Vector2f mousePos, sf::Text desired_option, sf::RenderWindow &window);
+        void draw(sf::RenderWindow&);
+        bool check_mousover(sf::Vector2f, sf::Text, sf::RenderWindow&);
         void set_item_position(sf::Text*, int, sf::RenderWindow&);
-        int getSelectedOption(sf::RenderWindow &window);
-        int *run(sf::RenderWindow &window, int curr_level);
-
+        int getSelectedOption(sf::RenderWindow&);
+        int *run(sf::RenderWindow&, int);
+        void set_GameLogic(GameLogic&);
 };
 
 #endif //CLEAR_MAINMENU_SCREEN_HPP
